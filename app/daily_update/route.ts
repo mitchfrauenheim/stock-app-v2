cd337689ba822d8143f8c0301da64d91ed156903
mitchfrauenheim <mitch.frauenheim@wustl.edu>
2026-02-11 14:50:41 -0800

    cron job test

--- app/daily_update/route.ts
+++ app/daily_update/route.ts
@@ -1,3 +1,21 @@
-export function GET(): Response {
-  return Response.json({ message: "daily update page" });
+import { NextResponse } from "next/server";
+
+async function dailyStockPriceUpdate(): Promise<void> {
+  console.log("hello world");
+}
+
+export function GET(request: Request): Response {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  try {
+    dailyStockPriceUpdate();
+    return Response.json({ message: "Daily update successful" });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+    return Response.json({ error: errorMessage }, { status: 500 });
+  }
 }
