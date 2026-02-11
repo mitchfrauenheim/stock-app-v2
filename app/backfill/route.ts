8d5c4eee2d6d5622c50c0fe24c1d5f8f2bfc2542
mitchfrauenheim <mitch.frauenheim@wustl.edu>
2026-02-11 14:22:56 -0800

    backfill script done

--- app/backfill/route.ts
+++ app/backfill/route.ts
@@ -6,15 +6,19 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function backfill(): Promise<void> {
   for (const stock of stocks) {
-    const stock_id =
+    const idResult =
       await sql`SELECT id FROM stocks WHERE symbol = ${stock.symbol}`;
+    const stockId: string = idResult[0].id;
     const data = await fetch(
       `https://api.twelvedata.com/time_series?symbol=${stock.symbol}&start_date=2026-01-01&interval=1day&apikey=${process.env.TWELVEDATA_API_KEY}`,
     );
     const stockData: TwelvedataTimeSeries = await data.json();
-    console.log(stockData.values);
-    // TODO: Map through the date values to create values to insert to stock_prices db table
+    await sql`
+      INSERT INTO stock_prices (stock_id, price_date, close_price)
+      VALUES ${sql(stockData.values.map((day) => [stockId, day.datetime, parseFloat(day.close)]))}
+    `;
 
+    console.log(`${stock.symbol} completed`);
     await new Promise((resolve) => setTimeout(resolve, 8000));
   }
 }
@@ -22,7 +26,7 @@ async function backfill(): Promise<void> {
 export async function GET(): Promise<Response> {
   try {
     await backfill();
-    return Response.json({ message: "backfill page" });
+    return Response.json({ message: "Stock data backfilled successfully" });
   } catch (error) {
     return Response.json({ error });
   }
